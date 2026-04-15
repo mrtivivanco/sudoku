6d758c4099b57fcde69951fddc96303e8292903b
titovivancoc <titovivancoc@gmail.com>
2026-04-15 16:54:23 -0400

    funciones

--- sudoku.c
+++ sudoku.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 
+
+void CeldasVacias(int tablero[N][N]){
+
+}
+
+int ProbarNumero(int tablero[N][N], int posx, int posy, int num){
+
+}
+//debe retornar 0 en caso de invalido y 1 en caso de valido
+
+int resolverSudoku(int tablero[N][N]){
+
+}
+//resuelve el sudoku
+
+
 int main() {
     int matriz[9][9];
-
-
-  return 0;
 }
